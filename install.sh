afd693fac63957e781994d3e23ea0d5983f1a4e3
Naitian Zhou <nzhou2011@gmail.com>
2017-05-19 21:34:31 -0400

    Add Airline Themes

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-mkdir -p $HOME/.vim
-mkdir -p $HOME/.tmux
+ln -s $PWD/vimrc/.vim $HOME/.vim
+ln -s $PWD/tmux/.tmux $HOME/.tmux
 
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 ln -s $PWD/vimrc/.vimrc $HOME/.vimrc
-ln -s $PWD/vimrc/.vim $HOME/.vim
 
 ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $PWD/tmux/.tmux $HOME/.tmux
 
 vim +PluginInstall +qall
 tmux source ~/.tmux.conf
+
+# Compile YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
